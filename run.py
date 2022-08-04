6a490e1299d62e7bc0a22c03952494dff37598b0
Jtune89 <Joseph.tune1@gmail.com>
2022-08-04 16:55:05 +0000

    updating and calculating stock data

--- run.py
+++ run.py
@@ -117,6 +117,21 @@ def get_last_5_entries_sales():
     
     return columns
 
+def calculate_stock_data(data):
+    """
+    calculating the stock required for next thing
+    """
+    print("Calculating stock data...\n")
+    new_stock_data = []
+
+    for column in data:
+        int_column = [int(num) for num in column]
+        average = sum(int_column) / len(int_column)
+        stock_num = average * 1.1
+        new_stock_data.append(round(stock_num))
+
+    return new_stock_data
+
 def main():
     """
     run all program functions
@@ -127,9 +142,12 @@ def main():
     calculate_surplus_data(sales_data)
     new_surplus_data = calculate_surplus_data(sales_data)
     update_worksheet(new_surplus_data, "surplus")
-
+    sales_columns = get_last_5_entries_sales()
+    stock_data = calculate_stock_data(sales_columns)
+    update_worksheet(stock_data, "stock")
 
 print ("welcome to Shop Stock Data Automation")
-#main()
+main()
 
 sales_columns = get_last_5_entries_sales()
+stock_data = calculate_stock_data(sales_columns)
